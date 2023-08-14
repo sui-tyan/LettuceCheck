1e108aa714950ed27c7b67008b7c5de2eaf456ee
Venn Edward Nicolas <vdn3903@students.uc-bcf.edu.ph>
2023-08-15 04:41:56 +0800

    add tailwind css

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,19 +1,19 @@
 import { Button, Text, View } from "react-native";
 
 function HomeScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Home screen</Text>
-      <Button
-        title="Take a Photo"
-        onPress={() => navigation.navigate("Take a Photo")}
-      />
-      <Button
-        title="Upload Image"
-        onPress={() => navigation.navigate("Upload Image")}
-      />
-    </View>
-  );
+	return (
+		<View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+			<Text>Home screen</Text>
+			<Button
+				title="Take a Photo"
+				onPress={() => navigation.navigate("Take a Photo")}
+			/>
+			<Button
+				title="Upload Image"
+				onPress={() => navigation.navigate("Upload Image")}
+			/>
+		</View>
+	);
 }
 
 export default HomeScreen;
